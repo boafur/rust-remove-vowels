579a104a74e9d11f5d65f6fa1cfab4c894e2155f
boafur <rubberboa01@gmail.com>
2020-11-08 21:06:12 -0800

    yes.

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 you must pass in arguments to replace ex.
 
 `cargo run hello world!` would output "hll wrld!"
+
+alternatively not supplying any arugments, `cargo run`, will prompt you at runtime for the string
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,19 @@ use std::env;
 
 fn main() {
   let mut args: Vec<String> = env::args().collect();
-  let mut send: String = String::from("");
+  let mut send: String = String::new();
   args.remove(0);
-  for x in args {
-    let y: &str = &format!(" {}", x);
-    send.push_str(&y);
+  if args.len() <= 0 {
+    println!("please enter a string:");
+    send = text_io::read!();
+  } else if args.len() > 0 {
+    for x in args {
+      let y: &str = &format!(" {}", x);
+      send.push_str(&y);
+    }
+    send = send.trim_start().to_string();
   }
-  let send2 = send.trim_start();
-  println!("new string with vowels removed: \"{}\"", replace_vowels(send2));
+  println!("new string with vowels removed: \"{}\"", replace_vowels(&send));
 }
 
 fn replace_vowels(s: &str) -> String {
